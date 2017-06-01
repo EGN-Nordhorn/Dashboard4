f2992fdb75efc6261bc19353e4ad5fb3cf1ca8a0
Hao Hu <hao.hu@sap.com>
2017-06-01 17:33:56 +0200

    add more UIs.

---
+++ DashboardNordhorn/MetalViewController.h
@@ -0,0 +1,13 @@
+//
+//  MetalViewController.h
+//  Dashboard4
+//
+//  Created by Hu, Hao on 01.06.17.
+//  Copyright © 2017 SAP SE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MetalViewController : UIViewController
+
+@end
