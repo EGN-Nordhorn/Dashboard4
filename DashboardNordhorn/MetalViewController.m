72ce376da0f8819e3f76d07b7156fea30be7d882
Hao Hu <hao.hu@sap.com>
2017-06-02 08:29:52 +0200

    metal is done. waiting for the data.

--- DashboardNordhorn/MetalViewController.m
+++ DashboardNordhorn/MetalViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "MetalViewController.h"
+#import "CircleView.h"
+#import "BluetoothSearchViewController.h"
 
-@interface MetalViewController ()
+
+
+@interface MetalViewController ()<BTSmartSensorDelegate>
+@property (weak, nonatomic) IBOutlet CircleView *jaView;
+@property (weak, nonatomic) IBOutlet UILabel *neinView;
+@property (strong, nonatomic) SerialGATT* serialGatt;
+@property(strong, nonatomic) NSMutableData* receivedData;
 
 @end
 
@@ -16,6 +24,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _receivedData =  [[NSMutableData alloc] init];
     // Do any additional setup after loading the view.
 }
 
@@ -24,14 +33,58 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
+- (void) setStatusView: (BOOL) flag {
+    
+    if (flag) {
+        //Ja View should be alert.
+        _jaView.alpha = 1.0;
+        _neinView.alpha = 0.2;
+    } else {
+        //Nein View should be alert.
+        _neinView.alpha = 0.2;
+        _jaView.alpha = 1.0;
+    }
+}
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    BluetoothSearchViewController* btVc = (BluetoothSearchViewController*)  segue.destinationViewController;
+    self.serialGatt = [[SerialGATT alloc] init];
+    [self.serialGatt setup];
+    btVc.serialGatt = self.serialGatt;
 }
-*/
+
+
+#pragma - Bluetooth
+
+- (void) serialGATTCharValueUpdated: (NSString *)UUID value: (NSData *)data{
+    
+    [self.receivedData appendData:data];
+    
+    UInt8 bytes_to_find[] = { 0x0D, 0x0A };
+    NSData *dataToFind = [NSData dataWithBytes:bytes_to_find
+                                        length:sizeof(bytes_to_find)];
+    
+    NSRange rangeOfData = [data rangeOfData:dataToFind options:0 range:NSMakeRange(0, data.length)];
+    
+    if (rangeOfData.location != NSNotFound) {
+        NSLog(@"Find the end");
+        NSString* stringData = [[NSString alloc] initWithData:[self.receivedData copy] encoding:NSUTF8StringEncoding];
+        [self.receivedData setLength:0];
+        NSLog(@" I got the string data is %@", stringData);
+    }
+    
+}
+- (void) setConnect{
+    NSLog(@"Bluetooth connected");
+}
+- (void) setDisconnect{
+    NSLog(@"Bluetooth disconnected!");
+    
+}
+
+
 
 @end
